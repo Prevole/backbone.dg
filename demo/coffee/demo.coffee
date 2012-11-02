4e178eae3f5153c7d5b5d5c8345d83ff32c27d87
Prevole <prevole@prevole.ch>
2012-11-02 08:11:45 +0100

    Working on demo (debugging)

--- demo/coffee/demo.coffee
+++ demo/coffee/demo.coffee
@@ -12,5 +12,30 @@ dataCollection = class extends Backbone.Collection
 
   filter: (model) ->
 
+headerView = (data) ->
+  return "<th>TestHeader</th>"
 
+rowView = (data) ->
+  return "<td>TestRow</td>"
 
+#Dg.registerTemplate("rowView", rowView)
+#Dg.registerTemplate("headerView", headerView)
+
+HeaderView = class extends Dg.HeaderView
+  template: headerView
+
+RowView = class extends Dg.RowView
+  template: rowView
+
+gridLayout = Dg.createDefaultLayout(
+  collection: new dataCollection()
+  gridRegions:
+    table:
+      view: Dg.TableView.extend
+        itemView: RowView
+        headerView: HeaderView
+)
+
+$(document).ready ->
+  layout = new gridLayout()
+  layout.render()
--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,38 +1,89 @@
-(function() {
-  var data, dataCollection, dataModel,
-    __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var HeaderView, RowView, data, dataCollection, dataModel, gridLayout, headerView, rowView,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  data = {};
+data = {};
 
-  dataModel = (function(_super) {
+dataModel = (function(_super) {
 
-    __extends(_Class, _super);
+  __extends(_Class, _super);
 
-    function _Class() {
-      return _Class.__super__.constructor.apply(this, arguments);
+  function _Class() {
+    return _Class.__super__.constructor.apply(this, arguments);
+  }
+
+  return _Class;
+
+})(Backbone.Model);
+
+dataCollection = (function(_super) {
+
+  __extends(_Class, _super);
+
+  function _Class() {
+    return _Class.__super__.constructor.apply(this, arguments);
+  }
+
+  _Class.prototype.model = dataModel;
+
+  _Class.prototype.comparator = function(model) {};
+
+  _Class.prototype.filter = function(model) {};
+
+  return _Class;
+
+})(Backbone.Collection);
+
+headerView = function(data) {
+  return "<th>TestHeader</th>";
+};
+
+rowView = function(data) {
+  return "<td>TestRow</td>";
+};
+
+HeaderView = (function(_super) {
+
+  __extends(_Class, _super);
+
+  function _Class() {
+    return _Class.__super__.constructor.apply(this, arguments);
+  }
+
+  _Class.prototype.template = headerView;
+
+  return _Class;
+
+})(Dg.HeaderView);
+
+RowView = (function(_super) {
+
+  __extends(_Class, _super);
+
+  function _Class() {
+    return _Class.__super__.constructor.apply(this, arguments);
+  }
+
+  _Class.prototype.template = rowView;
+
+  return _Class;
+
+})(Dg.RowView);
+
+gridLayout = Dg.createDefaultLayout({
+  collection: new dataCollection(),
+  gridRegions: {
+    table: {
+      view: Dg.TableView.extend({
+        itemView: RowView,
+        headerView: HeaderView
+      })
     }
+  }
+});
 
-    return _Class;
-
-  })(Backbone.Model);
-
-  dataCollection = (function(_super) {
-
-    __extends(_Class, _super);
-
-    function _Class() {
-      return _Class.__super__.constructor.apply(this, arguments);
-    }
-
-    _Class.prototype.model = dataModel;
-
-    _Class.prototype.comparator = function(model) {};
-
-    _Class.prototype.filter = function(model) {};
-
-    return _Class;
-
-  })(Backbone.Collection);
-
-}).call(this);
+$(document).ready(function() {
+  var layout;
+  layout = new gridLayout();
+  return layout.render();
+});
